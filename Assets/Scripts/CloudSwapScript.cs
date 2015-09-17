ed51ea98b3920e1b90e912543caa9741d88fdf63
dougen99 <dougen99@qq.com>
2015-09-17 22:38:55 +0800

    重新绘制了新的素材,正在进行云朵功能开发

---
+++ Assets/Scripts/CloudSwapScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+using System.Collections.Generic;
+
+public class CloudSwapScript : MonoBehaviour 
+{
+    public Transform[] clouds;              // 使用数组来容纳随机产生的云
+    public float maxSpeed = 2.0f;           // 云向下飘落的最大速度
+    public float minSpeed = 0.5f;           // 云向下飘落的最小速度
+    public float maxSize = 1.5f;            // 产生云的最大放大倍数
+    public float minSize = 1.0f;            // 产生云的最小放大倍数
+    public int maxCount = 5;                // 同屏内最多显示的云朵数量
+
+    private List<Transform> cloudPool;          // 用来存储已经没用的云对象池
+    private int cloudCount;                 // 当前屏幕内存在的云的数量
+
+	private void Start () 
+    {
+	    // 初始化变量
+        cloudPool = new List<Transform>();
+        cloudCount = 0;
+	}
+	
+	private void Update () 
+    {
+	    // 随机产生云的方式，总的来说以一定的速度往下移动，但是随机在一定范围内慢速的飘动
+        // 
+	}
+
+    // 用来生成新的云朵
+    private void SwapCloud()
+    {
+        // 先用随机函数生成本次云朵的类型，大小，下落速度等
+        int index = Random.Range(0, clouds.Length);
+        float speed = Random.Range(minSpeed, maxSpeed);
+        float size = Random.Range(minSize, maxSize);
+
+        // 查看cloudPool中是否还有可用的对象
+        foreach(var v in cloudPool)
+        {
+            // 从自己的脚本中得知类型
+        }
+    }
+}
---
+++ Assets/Scripts/MoveBarController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class MoveBarController : MonoBehaviour 
+{
+    public float gap = 1.5f;                        // 柱子之间的间距
+    public float horizontalSpeed = 3.0f;            // 柱子左右移动的速度 
+    public float verticalSpeed = -5.0f;              // 柱子下落的速度
+    public Vector2 initPos = new Vector2(0, 7f);    // 柱子的初始位置
+
+    public Transform leftBar;
+    public Transform rightBar;
+
+	private void Start () 
+    {
+	    // 初始化柱子的位置
+        transform.position = initPos;
+        leftBar.Translate(new Vector2(-gap/2f, 0), Space.Self);
+        rightBar.Translate(new Vector2(gap / 2f, 0), Space.Self);
+	}
+	
+
+	private void Update () 
+    {
+        // 当到达最右边时，将速度取反，往回走。
+	    if (transform.position.x >= 4f-gap/2f)
+        {
+            if (horizontalSpeed > 0)
+            {
+                horizontalSpeed = -horizontalSpeed;
+            }
+        }
+
+        // 当到达最左边时，将速度取反，往右走。
+        if (transform.position.x <= -4f + gap/2f)
+        {
+            if (horizontalSpeed < 0)
+            {
+                horizontalSpeed = -horizontalSpeed;
+            }
+        }
+
+        transform.Translate(new Vector2(horizontalSpeed * Time.deltaTime, verticalSpeed * Time.deltaTime));
+	}
+}
