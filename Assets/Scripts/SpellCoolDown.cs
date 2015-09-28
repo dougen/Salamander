568d77e0fb893c367c03ea0b2ab08aa337fda04f
dougen99 <dougen99@qq.com>
2015-09-28 22:34:26 +0800

    完成了基础的技能冷却功能。

---
+++ Assets/Scripts/SpellCoolDown.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SpellCoolDown : MonoBehaviour 
+{
+
+    public float timer;
+    public Text num;
+    public Image skill;
+
+	// Use this for initialization
+	private void Start () 
+    {
+        // 初始时，CD数字不显示。
+        if (num.enabled)
+        {
+            num.enabled = false;
+        }
+	}
+	
+	// Update is called once per frame
+	private void Update () 
+    {
+        // 按下空格键开始进入冷却状态
+	    if (Input.GetKeyDown(KeyCode.Space) && skill.fillAmount <= 0)
+        {
+            skill.fillAmount = 1f;
+        }
+
+        // 当开始计时时，显示CD剩余时间
+        if (skill.fillAmount <= 1f && skill.fillAmount > 0)
+        {
+            num.enabled = true;
+            skill.fillAmount -= 1f / timer * Time.deltaTime;
+            num.text = Mathf.CeilToInt(skill.fillAmount * timer).ToString();
+            if (num.text == "0")
+            {
+                num.enabled = false;
+            }
+        }
+	}
+}
