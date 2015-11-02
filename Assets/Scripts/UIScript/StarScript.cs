d225ab71e876d21f3c4c459aee4135ee36930bee
dougen99 <dougen99@qq.com>
2015-11-02 23:45:19 +0800

    增加开始界面（未完成）

---
+++ Assets/Scripts/UIScript/StarScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class StarScript : MonoBehaviour 
+{
+    public float rotSpeed;              //围绕Y轴旋转速度
+	// Use this for initialization
+	private void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	private void Update () 
+    {
+        transform.Rotate(Vector3.up, rotSpeed * Time.deltaTime, Space.Self);
+	}
+}
